598a893bd4c149aa990234fd9605dd31903a57a6
LucaStellato <lucastellato435@gmail.com>
2025-10-08 15:51:41 +0200

    task 4 bonus complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -32,20 +32,25 @@ for(i=0; i<teachers.length; i++)
 }
 
 
-
-
-const longNames = null;
-
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(5,1);
-console.log(teachers);
+
 
 
 
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.includes('Fabio');
+
+for(i=0; i<teachers.length; i++){
+  if(teachers[i]== 'Fabio'){
+
+    const isFabioPresent=teachers[i];
+    console.log(isFabioPresent);
+  }
+}
+
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join(",");
